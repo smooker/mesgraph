dd21a84b93c33ffc355715fb517c4727f54ea964
smooker <atvservice@mail.bg>
2022-03-17 04:58:30 +0200

    modified:   go.sh

--- go.sh
+++ go.sh
@@ -2,6 +2,8 @@
 #sed 's/[\x80-\xff]//g'
 #ef bb bf in front
 
+# example ./go.sh ./SLED_PODMIANA/it.csv
+
 
 iconv -f UTF-16LE -t UTF-8 $1 |sed 's/\r$//' | sed 's/\xef\xbb\xbf//g' | cut -f1-5 > start4.csv
 ./test2.pl
